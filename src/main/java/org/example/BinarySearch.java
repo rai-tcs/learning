3cf03483315d60fe53f8f91b2f6ed8a2ce4ed737
raghvendra <rai.raghvendra@tcs.com>
2026-01-07 17:08:44 +0530

    search insert position in a sorted array.

--- src/main/java/org/example/BinarySearch.java
+++ src/main/java/org/example/BinarySearch.java
@@ -21,6 +21,21 @@ public class BinarySearch {
         return -1;
     }
 
+    public static int searchInsertPosition(int[] arr, int searchValue){
+        int left = 0;
+        int right = arr.length; // right is exclusive
+        int mid = -1;
+        while(left<right){
+            mid = left + (right-left)/2;
+            if(arr[mid]<searchValue){
+                left = mid +1;
+            } else {
+                right = mid; // keep mid
+            }
+        }
+        return left;
+    }
+
     public static void main(String[] args){
         int[] arr = {1, 2, 2, 3, 4, 2, 3, 11, 10, 9};
         Arrays.sort(arr);
@@ -29,5 +44,8 @@ public class BinarySearch {
         int result = findIndex(arr, index);
         System.out.println("------- Binary Search Implementation");
         System.out.println("Index of "+index+" is : "+result);
+        int searchValue = 12;
+        int insertPos = searchInsertPosition(arr, searchValue);
+        System.out.println("Insert position of "+searchValue+" is : "+insertPos);
     }
 }
